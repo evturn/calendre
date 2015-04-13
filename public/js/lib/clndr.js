6e930714debb90b749b1f4580a553bdd5f27e5f8
anniebae <bae.annie@gmail.com>
2015-04-12 21:48:40 -0400

    messing around with clndr.js plugin to fix formatting of calendar

--- public/js/lib/clndr.js
+++ public/js/lib/clndr.js
@@ -39,7 +39,7 @@
 
   // This is the default calendar template. This can be overridden.
   var clndrTemplate = "<div class='clndr-controls'>" +
-    "<div class='clndr-control-button'><span class='clndr-previous-button'>previous</span></div><div class='month'><%= month %> <%= year %></div><div class='clndr-control-button rightalign'><span class='clndr-next-button'>next</span></div>" +
+    "<div class='clndr-control-button'><button class='clndr-previous-button'>previous</button></div><div class='month'><%= month %> <%= year %></div><div class='clndr-control-button rightalign'><button class='clndr-next-button'>next</button></div>" +
     "</div>" +
   "<table class='clndr-table' border='0' cellspacing='0' cellpadding='0'>" +
     "<thead>" +
